df1719ccf368ec36ffca11945a16e2fa6486ddd7
olalesam <olalesam87@gmail.com>
2025-09-09 13:05:26 +0000

    redirect full path /produits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
 export const routes: Routes = [
-  {path: "produits", component: ProduitsComponent}
+  {path: "produits", component: ProduitsComponent},
+  {path: "produits", component: AddProduitComponent},
+   {path: "", redirectTo: "produits", pathMatch: "full"}
 ];
