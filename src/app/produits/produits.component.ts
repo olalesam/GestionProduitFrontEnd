2c0485edd7c6d957b30bdb2414b05c565b61256b
olalesam <olalesam87@gmail.com>
2025-09-09 14:01:23 +0000

    create product model

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,17 +1,36 @@
-
 import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './produits.component.html',
-  styleUrl: './produits.component.css'
+  styleUrl: './produits.component.css',
 })
 export class ProduitsComponent {
-  produits : String[];
+  produits: Produit[]; // tableau de produit
 
   constructor() {
-    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+    this.produits = [
+      {
+        idProduit: 1,
+        nomProduit: 'PC Asus',
+        prixProduit: 3000.6,
+        dateCreation: new Date('01/14/2011'),
+      },
+      {
+        idProduit: 2,
+        nomProduit: 'Imprimante Epson',
+        prixProduit: 450,
+        dateCreation: new Date('12/17/2010'),
+      },
+      {
+        idProduit: 3,
+        nomProduit: 'Tablette Samsung',
+        prixProduit: 900.123,
+        dateCreation: new Date('02/20/2020'),
+      },
+    ];
   }
-
 }
