a172a6ba3a954d16b32f18b09ca61f5f39e017cc
olalesam <olalesam87@gmail.com>
2025-09-09 13:17:32 +0000

    create component add-produit

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,11 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-produits',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './produits.component.html',
   styleUrl: './produits.component.css'
 })
 export class ProduitsComponent {
+  produits : String[];
+
+  constructor() {
+    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+  }
 
 }
