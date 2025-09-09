365a3a465baa5d21846ae533405833141c00bb9e
olalesam <olalesam87@gmail.com>
2025-09-09 12:57:28 +0000

    create produits component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProduitsComponent } from './produits/produits.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "produits", component: ProduitsComponent}
+];
